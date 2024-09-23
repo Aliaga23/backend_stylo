c06da3c32d1c522b3e85d3bb841b7be4f90a15a2
cryobus <jaav2801@gmail.com>
2024-09-22 22:31:24 -0400

    CU3

--- src/main/java/com/stylo/backend/config/CorsConfig.java
+++ src/main/java/com/stylo/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Aplica CORS a todos los endpoints
-                .allowedOrigins("frontendstylo-production.up.railway.app")  // Permitir solicitudes solo desde localhost:3000
+                .allowedOrigins("https://frontendstylo-production.up.railway.app")  // Permitir solicitudes solo desde Railway frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Permitir estos métodos HTTP
                 .allowedHeaders("*")  // Permitir todos los headers
                 .allowCredentials(true);  // Permitir el uso de credenciales (cookies, autenticación, etc.)
